a43b3c18a254128968d978b504eec20916695ef1
Steve Workman <steve.workman@maersk.com>
2023-10-01 14:49:24 +0100

    feat: added getStationDetail API call and corrected types

--- demo.js
+++ demo.js
@@ -4,3 +4,13 @@ const api = solisApi(process.env.BASEURL, process.env.KEY, process.env.SECRET);
 const stationList = await api.getUserStationList();
 
 console.log(stationList.data.page.records[0]);
+
+console.log(
+  "Getting station data for station id: " + stationList.data.page.records[0].id
+);
+
+const stationData = await api.getStationDetail(
+  stationList.data.page.records[0].id
+);
+
+console.log(stationData);
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import { makeAPICall } from "./api.js";
 import { config } from "dotenv";
 import type {
+  PowerStationDetails,
   SolisAPIResponse,
   UserStationListResponse,
 } from "./types/apiResponse.js";
 config();
 
-const userStationList = {
-  verb: "POST",
-  uri: "/v1/api/userStationList",
-} as {
+export type APIURIConfig = {
   verb: "POST" | "GET";
   uri: string;
+};
+
+const userStationList: APIURIConfig = {
+  verb: "POST",
+  uri: "/v1/api/userStationList",
+};
+const stationDetail: APIURIConfig = {
+  verb: "POST",
+  uri: "/v1/api/stationDetail",
 };
 
 export default function (baseUrl: string, key: string, secret: string) {
@@ -19,12 +26,25 @@ export default function (baseUrl: string, key: string, secret: string) {
     getUserStationList: async (
       pageNo = 1,
       pageSize = 20,
-      NmiCode: string
+      NmiCode?: string
     ): Promise<SolisAPIResponse<UserStationListResponse>> => {
       return await makeAPICall(
         userStationList.verb,
         userStationList.uri,
         { pageNo, pageSize, NmiCode },
+        key,
+        secret,
+        baseUrl
+      );
+    },
+    getStationDetail: async (
+      id: string, // Power station ID
+      nmiCode?: string // NMI code
+    ): Promise<SolisAPIResponse<PowerStationDetails>> => {
+      return await makeAPICall(
+        stationDetail.verb,
+        stationDetail.uri,
+        { id, nmiCode },
         key,
         secret,
         baseUrl
