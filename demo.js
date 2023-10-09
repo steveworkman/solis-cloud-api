cbdfe78e835c86dce63d0e655db0ad10b1e9b3a4
Steve Workman <steve.workman@maersk.com>
2023-10-09 22:29:44 +0100

    test: updated demo js file

--- demo.js
+++ demo.js
@@ -3,7 +3,7 @@ import solisApi from "./dist/index.js";
 const api = solisApi(process.env.BASEURL, process.env.KEY, process.env.SECRET);
 const stationList = await api.getUserStationList();
 
-console.log(stationList.data.page.records[0]);
+// console.log(stationList.data.page.records[0]);
 
 console.log(
   "Getting station data for station id: " + stationList.data.page.records[0].id
@@ -13,4 +13,14 @@ const stationData = await api.getStationDetail(
   stationList.data.page.records[0].id
 );
 
-console.log(stationData);
+// console.log(stationData);
+
+console.log("PV Power:", stationData.data.generatorPower);
+console.log(`Battery: ${stationData.data.batteryPercent}%`);
+console.log(
+  "Battery is",
+  stationData.data.batteryPower > 0 ? "charging" : "discharging"
+);
+console.log(
+  `Power needed ${stationData.data.familyLoadPower}${stationData.data.familyLoadPowerStr}`
+);
