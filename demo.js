9bd9a515a05ab168ce3d0652d2184d8e30508999
Steve Workman <steve.workman@maersk.com>
2023-11-24 09:06:55 +0000

    fix: move depdendencies out of devDependencies

--- demo.js
+++ demo.js
@@ -5,40 +5,42 @@ const stationList = await api.getUserStationList();
 
 // console.log(stationList.data.page.records[0]);
 
-console.log(
-  "Getting station data for station id: " + stationList.data.page.records[0].id
-);
-const stationId = stationList.data.page.records[0].id;
+stationList.data.page.records.forEach(async (station) => {
+  console.log("Getting station data for station id: " + station.id);
+  const stationId = station.id;
 
-const stationData = await api.getStationDetail(stationId);
+  const stationData = await api.getStationDetail(stationId);
 
-// console.log(stationData);
+  console.log("Station ID:", station.id);
+  console.log("PV Power:", stationData.data.power);
+  if (stationData.data.batteryPower !== 0) {
+    console.log(`Battery: ${stationData.data.batteryPercent}%`);
+    console.log(
+      "Battery is",
+      stationData.data.batteryPower > 0 ? "charging" : "discharging"
+    );
+  }
 
-console.log("PV Power:", stationData.data.generatorPower);
-console.log(`Battery: ${stationData.data.batteryPercent}%`);
-console.log(
-  "Battery is",
-  stationData.data.batteryPower > 0 ? "charging" : "discharging"
-);
-console.log(
-  `Power needed ${stationData.data.familyLoadPower}${stationData.data.familyLoadPowerStr}`
-);
+  console.log(
+    `Power needed ${stationData.data.familyLoadPower}${stationData.data.familyLoadPowerStr}`
+  );
+});
 
-const dataloggerList = await api.getDataloggerList(stationId);
-console.log(dataloggerList.data.page.records);
+// const dataloggerList = await api.getDataloggerList(stationId);
+// console.log(dataloggerList.data.page.records);
 
-const dataLogger = await api.getDataloggerDetail(
-  dataloggerList.data.page.records[0].id
-);
-console.log(dataLogger.data);
+// const dataLogger = await api.getDataloggerDetail(
+//   dataloggerList.data.page.records[0].id
+// );
+// console.log(dataLogger.data);
 
-const inverterList = await api.getInverterList(stationId);
-console.log(inverterList.data.page.records);
+// const inverterList = await api.getInverterList(stationId);
+// console.log(inverterList.data.page.records);
 
-const inverter = await api.getInverterDetail(
-  inverterList.data.page.records[0].id
-);
-console.log(inverter.data);
+// const inverter = await api.getInverterDetail(
+//   inverterList.data.page.records[0].id
+// );
+// console.log(inverter.data);
 
-const stationDay = await api.getStationDay(stationId, "GBP", "2023-10-09", 0);
-console.log(stationDay);
+// const stationDay = await api.getStationDay(stationId, "GBP", "2023-10-09", 0);
+// console.log(stationDay);
